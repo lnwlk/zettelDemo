937d6b446f36319c851b60c6a15be6ce7e6f997f
lnwlk <hello@lenawilke.de>
2025-11-24 10:13:18 +0100

    text update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
   referenceText:
-    "Zielsetzung:Im Rahmen der digitalen Transformation entwickeln wir eine mobile Applikation, deren primäre Zielsetzung in der Vereinfachung des Zugangs zu behördlicher Kommunikation liegt. zetteln ist eine Applikation die offizielle Texte in verständliche Sprache wiedergibt. Die Applikation erläutert amtlicher Schriftstücke durch Kontextualisierung und bietet eine adaptive Ausfüllhilfen für Antragsverfahren. Zudem unterstützt die  App ihre Nutzer:innen um strukturiert analoge Dokumente zu sortieren, zu bearbeiten und zu digitalisieren. Der Text ist schwierig? Teste die App auf demo.zetteln.app",
+    "Zielsetzung: Im Rahmen der digitalen Transformation entwickeln wir eine mobile Applikation, deren primäre Zielsetzung in der Vereinfachung des Zugangs zu behördlicher Kommunikation liegt. zetteln ist eine Applikation die offizielle Texte in verständliche Sprache wiedergibt. Die Applikation erläutert amtliche Schriftstücke durch Kontextualisierung und bietet eine adaptive Ausfüllhilfen für Antragsverfahren. Nutzer und Nutzerinnen können unterstützt durch die Applikation strukturiert analoge Dokumente sortieren, bearbeiten und digitalisieren. Der Text ist schwierig? Teste die App auf demo.zetteln.app",
   matchThreshold: 0.6,
 };
